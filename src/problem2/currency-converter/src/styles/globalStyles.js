2fdd9fc2534637102509781a5a78e1f90e98e894
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2024-09-09 20:46:19 +0100

    style: globalStyles.js

---
+++ src/problem2/currency-converter/src/styles/globalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #e9ecef;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+export default GlobalStyle;
